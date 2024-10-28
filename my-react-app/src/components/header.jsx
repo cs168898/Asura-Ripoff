9cd2bc5c4dcafee737964c6a2421a6f6c5ebe9dc
Samuel Yew Han Sen <135582318+cs168898@users.noreply.github.com>
2024-10-28 13:08:26 +0800

    added header and changed some css (incomplete)

---
+++ my-react-app/src/components/header.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+
+function Header(){
+ //Insert javascript functionality here
+ 
+    return(
+        <>
+        <div className='header-wrapper'>
+            <div className='logo'>
+                enter logo here
+            </div>
+            <div className='nav-bar'>
+                <nav>
+                <ul>
+                    <li><a href="default.asp">Home</a></li>
+                    <li><a href="news.asp">Bookmark</a></li>
+                    <li><a href="contact.asp">Comics</a></li>
+                </ul>
+                </nav>
+            </div>
+            <div className='searchbar-and-avatar'>
+
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Header;
---
+++ my-react-app/src/pages/home.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import Header from '../components/header'
+
+function Home(){
+ //Insert javascript functionality here
+
+    return(
+        <>
+        <header>
+            <Header/>
+        </header>
+        
+        </>
+    )
+}
+
+export default Home; // Ensure this is a default export
