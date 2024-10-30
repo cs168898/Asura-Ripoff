2b8d0c6a441d54046b40cf4195daff90d075abb9
Samuel Yew Han Sen <samuelyew98@gmail.com>
2024-10-30 19:51:32 +0800

    Added legal page format and its css

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Specific_Comic from './pages/specific-Comic';
-
+import Legal from './pages/legal';
 
 function App() {
 
@@ -14,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="specific-comic" element={<Specific_Comic />}/>
+        <Route path="legal" element={<Legal />}/>
       </Routes>
     </Router>
   )
